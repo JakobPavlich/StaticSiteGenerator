72434f3d4aece027a1f5c2f6b0efa7cb80f44da8
JakobPavlich <pavlich.jakob@gmail.com>
2026-06-29 08:34:04 +0200

    za zdaj dela, da izbriše in ustvari nov public, in pa ugotovi ali je datoteka ali mapa v static za vse elemente

--- src/copy_directory.py
+++ src/copy_directory.py
@@ -1,11 +1,20 @@
 import os
+import shutil
 
 
 def copy_directory():
     cwd = os.getcwd()
-    print(cwd)
-    if os.path.exists(os.path.abspath("static")):
-        print("folder 'static' exists")
+    static_path = os.path.abspath("static")
+    public_path = os.path.abspath("public")
 
-    if os.path.exists(os.path.abspath("public")):
-        print("folder 'public' exists")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
+
+    static_dir = os.listdir(static_path)
+    for item in static_dir:
+        item_path = os.path.normpath(os.path.join(cwd, f"static/{item}"))
+        if os.path.isfile(item_path):
+            print(f"{item} is a file")
+        else:
+            print(f"{item} is a directory")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-from textnode import TextNode, TextType
+# from textnode import TextNode, TextType
 from copy_directory import copy_directory
 
 
 def main():
-    text_node = TextNode(
-        "Hope it works", TextType.LINK, "https://www.google.com")
-    print(text_node)
-
+    print("---copy_directory.py:")
     copy_directory()
 
 
