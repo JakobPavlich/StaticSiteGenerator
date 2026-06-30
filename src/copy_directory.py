215db097a69ea9bd025f50efddcb863221726cbf
JakobPavlich <pavlich.jakob@gmail.com>
2026-06-30 07:14:15 +0200

    Zgleda še nisem shranil vse od včeraj

--- src/copy_directory.py
+++ src/copy_directory.py
@@ -2,21 +2,22 @@ import os
 import shutil
 
 
-def copy_directory(relative_path: str = None):
+def copy_directory(source_path: str = None, destination_path: str = None):
     # cwd = os.getcwd()
-    public_path = os.path.abspath("public")
-    if relative_path:
-        source_path = os.path.abspath("static" + relative_path)
-        destination_path = os.path.abspath("public" + relative_path)
-        if not os.path.exists(destination_path):
-            os.mkdir(destination_path)
-    else:
-        relative_path = ""
-        destination_path = os.path.abspath("public")
-        source_path = os.path.abspath("static")
-        if os.path.exists(public_path):
-            shutil.rmtree(public_path)
-        os.mkdir(public_path)
+    # public_path = os.path.abspath("public")
+    # if relative_path:
+    #     # source_path = os.path.abspath("static" + relative_path)
+    #     source_path = relative_path
+    #     destination_path = os.path.abspath("public" + relative_path)
+    if not os.path.exists(destination_path):
+        os.mkdir(destination_path)
+    # else:
+        # relative_path = ""
+        # destination_path = os.path.abspath("public")
+        # source_path = os.path.abspath("static")
+    # if os.path.exists(public_path):
+    #     shutil.rmtree(public_path)
+    # os.mkdir(public_path)
 
     static_dir = os.listdir(source_path)
     for item in static_dir:
@@ -26,5 +27,9 @@ def copy_directory(relative_path: str = None):
             shutil.copy(item_path, destination_path)
         else:
             dir = item
-            relative_path += f"/{dir}"
-            copy_directory(relative_path)
+            # print(f"-Looking at the directory: {dir}")
+            # os.mkdir(os.path.normpath(os.path.join(destination_path, dir)))
+            new_source_path = os.path.normpath(os.path.join(source_path, dir))
+            new_destination_path = os.path.normpath(
+                os.path.join(destination_path, dir))
+            copy_directory(new_source_path, new_destination_path)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 # from textnode import TextNode, TextType
 from copy_directory import copy_directory
+import os
+import shutil
 
 
 def main():
+
+    destination_path = os.path.abspath("public")
+    source_path = os.path.abspath("static")
     print("---copy_directory.py:")
-    copy_directory()
+    public_path = os.path.abspath("public")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
+    copy_directory(source_path, destination_path)
 
 
 main()
