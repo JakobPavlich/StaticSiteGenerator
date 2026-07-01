67ae4025d6422cf59f65b9570bb75b199193143d
JakobPavlich <pavlich.jakob@gmail.com>
2026-07-01 09:53:54 +0200

    Zdi se, da stran deluje, tudi z več stranmi in linki tudi zdaj delujejo, ko ni več v public/content ampak v public/

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from copy_directory import copy_directory
 import os
 import shutil
-from webpage_functions import generate_page
+from webpage_functions import generate_page, generate_pages_recursive
 
 
 def main():
@@ -15,7 +15,12 @@ def main():
         shutil.rmtree(public_path)
     os.mkdir(public_path)
     copy_directory(source_path, destination_path)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
+    content_path = os.path.abspath("content")
+    public_content_path = os.path.normpath(
+        os.path.join(public_path, "content"))
+    generate_pages_recursive(
+        content_path, "template.html", public_path)
 
 
 main()
--- src/webpage_functions.py
+++ src/webpage_functions.py
@@ -37,3 +37,40 @@ def generate_page(from_path, template_path, dest_path):
 
     except Exception as e:
         print(f"Error: {e}")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    try:
+        content_dir = os.listdir(dir_path_content)
+        for item in content_dir:
+            item_path = os.path.normpath(os.path.join(dir_path_content, item))
+            if os.path.isfile(item_path) and item[-3:] == ".md":
+                with open(item_path) as md_item:
+                    md_file = md_item.read()
+                with open(template_path) as t:
+                    template = t.read()
+                html_node = markdown_to_html_node(md_file)
+                html_content = html_node.to_html()
+                title = extract_title(md_file)
+                html_with_title = template.replace("{{ Title }}", title)
+                full_html = html_with_title.replace(
+                    "{{ Content }}", html_content)
+
+                dest_html_file_path = os.path.normpath(
+                    os.path.join(dest_dir_path, (item[:-3] + ".html")))
+                parent_dir = os.path.dirname(dest_html_file_path)
+                os.makedirs(parent_dir, exist_ok=True)
+                with open(dest_html_file_path, "w") as f:
+                    f.write(full_html)
+
+                print(f'Successfully wrote to "{dest_html_file_path}"')
+            elif os.path.isfile(item_path) and item[-3:] != ".md":
+                continue
+            else:
+                new_dest_dir_path = os.path.normpath(
+                    os.path.join(dest_dir_path, item))
+                generate_pages_recursive(
+                    item_path, template_path, new_dest_dir_path)
+
+    except Exception as e:
+        print(f"Error: {e}")
